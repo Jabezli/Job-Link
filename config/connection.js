e9d97e488bbd0c1fc5004194add2d0977f020aeb
Matthew <unicomatthew@gmail.com>
2023-03-28 20:45:05 -0700

    added sign up for login" ;

--- config/connection.js
+++ config/connection.js
@@ -7,13 +7,17 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    // process.env.DB_NAME,
+    // process.env.DB_USER,
+    // process.env.DB_PASSWORD,
+     DB_NAME='employee_db',
+    DB_USER='root',
+    DB_PASSWORD='Mw113741@',
     {
       host: "localhost",
       dialect: "mysql",
       port: 3306,
+      
     }
   );
 }
--- public/js/Login.js
+++ public/js/Login.js
@@ -1,16 +1,36 @@
 const btnLoginSumit = document.querySelector('.btn-Submit');
 const toastLiveExample = document.getElementById('liveToast')
 const btnSignUP = document.querySelector('.btn-SignUp')
+const SignUpForm = document.querySelector('.signupSelector');
+const hi = document.querySelector('.signuplink');
+const pageloginForm = document.querySelector('.loginform');
+SignUpForm.style.display = 'none';
+hi.addEventListener('click', displayForms)
+async function displayForms(){
+    //  ageloginForm.style.display = 'none';
+    //  SignUppForm.style.display = 'block';
+
+     if (pageloginForm.style.display === 'none') {
+        pageloginForm.style.display = 'block';
+        SignUpForm.style.display = 'none';
+      } else {
+        pageloginForm.style.display = 'none';
+        SignUpForm.style.display = 'block';
+      }
+
+
+   
+}
 
 const loginForm = async (event) => {
     event.preventDefault();
     const username = document.querySelector('.username').value.trim();
     const password = document.querySelector('.password').value.trim();
-if (password == "") {
-   btnLoginSumit.textContent = 'both text fields must be filled'
- return
+    if (password == "") {
+        btnLoginSumit.textContent = 'both text fields must be filled'
+        return
 
-}
+    }
 
     if (username && password) {
         const response = await fetch('http://localhost:3001/api/user/login', {
@@ -29,17 +49,17 @@ if (password == "") {
 
 async function signUp(e) {
     e.preventDefault();
-    const username = document.querySelector('.username').value.trim();
-    const password = document.querySelector('.password').value.trim();
+    const username = document.querySelector('.Signusername').value.trim();
+    const password = document.querySelector('.Signpassword').value.trim();
     if (username && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('http://localhost:3001/api/user', {
             method: 'POST',
             body: JSON.stringify({ password, username }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
-            document.location.replace('/');
+            document.location.replace('/login');
         } else {
             alert('could not to sign up.');
         }
@@ -49,4 +69,4 @@ async function signUp(e) {
 
 
 btnLoginSumit.addEventListener('click', loginForm);
-// btnSignUP.addEventListener('click', signUp)
+ btnSignUP.addEventListener('click', signUp)
