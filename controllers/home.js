a383b6a5ecf729ae00a743d632b4672525507a87
Matthew <unicomatthew@gmail.com>
2023-03-30 14:28:19 -0700

    fixed fetch views

--- controllers/home.js
+++ controllers/home.js
@@ -24,7 +24,7 @@ router.get('/', async (req, res) => {
 
     const mutijobs = jobData.map((job) => {
         const formattedJob = job.get({ plain: true });
-        formattedJob.createdAt = moment(job.createdAt).calendar();
+        formattedJob.createdAt = moment(job.createdAt).format("MMM Do YY");
         formattedJob.updatedAt = moment(job.updatedAt).startOf('day').fromNow()
         return formattedJob;
     });
