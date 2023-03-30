50339031e8aa7d555ba019dd6284bca263bdbe7b
Matthew <unicomatthew@gmail.com>
2023-03-30 14:30:18 -0700

    fixed to force true

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(routes);
 
 // turn on connection to db and server
 //force is to drop DB
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log(`Now listening${PORT}`));
 });
 
